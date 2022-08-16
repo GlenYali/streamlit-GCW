d2ca15e12a1e805bd8ecde88256738bb3bd874ef
GlenYali <102636664+GlenYali@users.noreply.github.com>
2022-08-16 21:49:49 +1000

    Update planet.py

--- apps/planet.py
+++ apps/planet.py
@@ -8,24 +8,17 @@ def app():
 
     st.title("Planet Imagery")
 
-    from requests.auth import HTTPBasicAuth
-# import helper functions to make Basic request to Planet API
+    os.environ["31848fad673b405f9c7eee934f1d2ad3"] = "12345"
+    tile_format = "ipyleaflet"
 
-    PLANET_API_KEY = os.getenv("31848fad673b405f9c7eee934f1d2ad3")
-# Setup the API Key from the `PL_API_KEY` environment variable
+    if os.environ.get("USE_FOLIUM") is not None:
+        tile_format = "folium"
+        # leafmap.planet_monthly()
+        monthly_tiles = leafmap.planet_monthly_tiles(tile_format=tile_format)
+        for tile in monthly_tiles:
+            print(tile)
 
-    BASE_URL = 'https://api.planet.com/basemaps/v1/mosaics'
-
-    if PLANET_API_KEY is None:
-       PLANET_API_KEY = '12345'
-#pass in your API key
-
-    auth = HTTPBasicAuth(PLANET_API_KEY, '')
-# HTTPBasicAuth() wants a username & password; you can pass an empty string for the password
-
-    res = requests.get(url=BASE_URL, auth=auth)
-
-      print(res.status_code)
-# make a request to the Basemaps API and test the response
+    m = leafmap.Map()
+    m.add_planet_by_month(year=2022, month=7, api_key="31848fad673b405f9c7eee934f1d2ad3")
 
     
