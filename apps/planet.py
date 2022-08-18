8faca4d23d8afd75b877e6d5ed5e2f6103153e1f
GlenYali <102636664+GlenYali@users.noreply.github.com>
2022-08-19 03:08:06 +1000

    Update planet.py

--- apps/planet.py
+++ apps/planet.py
@@ -1,30 +1,12 @@
 import os
-import requests
 import leafmap.foliumap as leafmap
 import streamlit as st
-from requests.auth import HTTPBasicAuth
 
 def app():
 
     st.title("Planet Imagery")
-    
-    PLANET_API_KEY = os.getenv("PL_API_KEY")
-    
-    BASE_URL = 'https://api.planet.com/basemaps/v1/mosaics'
-    
-    if PLANET_API_KEY is None:
-    PLANET_API_KEY = '12345'
-    #pass in your API key
-    
-    auth = HTTPBasicAuth(PLANET_API_KEY, '')
-    # HTTPBasicAuth() wants a username & password; you can pass an empty string for the password
-    
-    res = requests.get(url=BASE_URL, auth=auth)
-        print(res.status_code)
-        # make a request to the Basemaps API and test the response
         
-    
-    #os.environ["PL_API_KEY"] = "12345"
+    os.environ["PL_API_KEY"] = "12345"
     tile_format = "ipyleaflet"
 
     if os.environ.get("USE_FOLIUM") is not None:
