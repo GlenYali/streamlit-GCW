1fba022f5623c28561424209d924d9d0aef26090
GlenYali <102636664+GlenYali@users.noreply.github.com>
2022-08-19 10:52:13 +1000

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -8,12 +8,13 @@ def app():
     st.markdown(
         """
     A [streamlit](https://streamlit.io) app for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
-    To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
-    https://share.streamlit.io/giswqs/streamlit-template?page=upload
-
+    
     """
     )
 
+    #To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
+    #https://share.streamlit.io/giswqs/streamlit-template?page=upload
+        
     m = leafmap.Map(locate_control=True)
     m.add_basemap("ROADMAP")
     m.to_streamlit(height=700)
