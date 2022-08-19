c67f63476c1a218b1fcee9ff1375fa728caabc8c
GlenYali <102636664+GlenYali@users.noreply.github.com>
2022-08-19 10:54:08 +1000

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -7,11 +7,11 @@ def app():
 
     st.markdown(
         """
-    A [streamlit](https://streamlit.io) app for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
+    A [streamlit] app for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
     
     """
     )
-
+    #A [streamlit](https://streamlit.io) app for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
     #To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
     #https://share.streamlit.io/giswqs/streamlit-template?page=upload
         
